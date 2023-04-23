99afe802203474d00774cd190c526401bd6a0c01
rambms <rambms07@gmail.com>
2023-04-23 19:38:11 +0530

    ok done

--- HelloJava/src/gitWork/FirstPrg.java
+++ HelloJava/src/gitWork/FirstPrg.java
@@ -3,7 +3,7 @@ package gitWork;
 public class FirstPrg {
 
 	public static void main(String[] args) {
-		System.out.println("Hello sir ji final");
+		System.out.println("Hello sir ji final hai just avi");
 
 	}
 
